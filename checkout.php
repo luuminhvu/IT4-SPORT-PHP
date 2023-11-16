33255b8cc80f636811a9064829667a9bf5057cbb
Luu Vu <vu7178@gmail.com>
2023-11-16 10:12:34 +0700

    checkout

--- checkout.php
+++ checkout.php
@@ -122,20 +122,20 @@ if(!isset($_SESSION['cart_p_id'])) {
                             <td><?php echo $arr_cart_p_name[$i]; ?></td>
                             <td><?php echo $arr_cart_size_name[$i]; ?></td>
                             <td><?php echo $arr_cart_color_name[$i]; ?></td>
-                            <td><?php echo  $languages[1]; ?><?php echo $arr_cart_p_current_price[$i]; ?></td>
+                            <td><?php echo $arr_cart_p_current_price[$i];?><?php echo  $languages[1]; ?></td>
                             <td><?php echo $arr_cart_p_qty[$i]; ?></td>
                             <td class="text-right">
                                 <?php
                                 $row_total_price = $arr_cart_p_current_price[$i]*$arr_cart_p_qty[$i];
                                 $table_total_price = $table_total_price + $row_total_price;
                                 ?>
-                                <?php echo  $languages[1]; ?><?php echo $row_total_price; ?>
+                                <?php echo $row_total_price; ?><?php echo  $languages[1]; ?>
                             </td>
                         </tr>
                         <?php endfor; ?>
                         <tr>
                             <th colspan="7" class="total-text"><?php echo  $languages[81]; ?></th>
-                            <th class="total-amount"><?php echo  $languages[1]; ?><?php echo $table_total_price; ?></th>
+                            <th class="total-amount"><?php echo $table_total_price; ?><?php echo  $languages[1]; ?></th>
                         </tr>
                         <?php
                         $statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost WHERE country_id=?");
@@ -157,7 +157,7 @@ if(!isset($_SESSION['cart_p_id'])) {
                         ?>
                         <tr>
                             <td colspan="7" class="total-text"><?php echo  $languages[84]; ?></td>
-                            <td class="total-amount"><?php echo  $languages[1]; ?><?php echo $shipping_cost; ?></td>
+                            <td class="total-amount"><?php echo $shipping_cost; ?><?php echo  $languages[1]; ?></td>
                         </tr>
                         <tr>
                             <th colspan="7" class="total-text"><?php echo  $languages[82]; ?></th>
@@ -165,7 +165,7 @@ if(!isset($_SESSION['cart_p_id'])) {
                                 <?php
                                 $final_total = $table_total_price+$shipping_cost;
                                 ?>
-                                <?php echo  $languages[1]; ?><?php echo $final_total; ?>
+                                <?php echo $final_total; ?><?php echo  $languages[1]; ?>
                             </th>
                         </tr>
                     </table>
